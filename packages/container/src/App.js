0709fb123a32c6197b6faa1f9348b43c1432e4d0
Nuno Bettencourt <nuno.bettencourt@playtech.com>
2022-08-09 14:50:32 +0100

    Forcing deployments

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
-  return (
-    <div>
-      <h1>Hi there!!!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
-  );
+	return (
+		<div>
+			<h1>Hi there!!</h1>
+			<hr />
+			<MarketingApp />
+		</div>
+	);
 };
