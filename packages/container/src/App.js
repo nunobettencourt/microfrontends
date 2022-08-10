7f05b20b83c586708d4d7893fab2fa02f20b9a5c
Nuno Bettencourt <nuno.bettencourt@playtech.com>
2022-08-10 09:36:25 +0100

    Randomise production classNames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, CreateGenerateClassName } from '@material-ui/styles';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
+
+const generateClassName = CreateGenerateClassName({
+	productionPrefix: 'co',
+});
 
 export default () => {
 	return (
 		<BrowserRouter>
-			<div className="container-app">
-				<Header />
-				<MarketingApp />
-			</div>
+			<StylesProvider generateClassName={generateClassName}>
+				<div className="container-app">
+					<Header />
+					<MarketingApp />
+				</div>
+			</StylesProvider>
 		</BrowserRouter>
 	);
 };
