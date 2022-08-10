1823bdbfb973ca89bbea47ae97d19c0597b5f761
Nuno Bettencourt <nuno.bettencourt@playtech.com>
2022-08-10 09:39:25 +0100

    Fixed typo in container App.js

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { StylesProvider, CreateGenerateClassName } from '@material-ui/styles';
+import {
+	StylesProvider,
+	createGenerateClassName,
+} from '@material-ui/core/styles';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
-const generateClassName = CreateGenerateClassName({
+const generateClassName = createGenerateClassName({
 	productionPrefix: 'co',
 });
 
