e45f66a1e40b50dec8dfa2f98bf121cabae470ca
Nuno Bettencourt <nuno.bettencourt@playtech.com>
2022-08-11 11:26:44 +0100

    Updated prod configs for container

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -6,20 +6,21 @@ const packageJson = require('../package.json');
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
-  mode: 'production',
-  output: {
-    filename: '[name].[contenthash].js',
-    publicPath: '/container/latest/',
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-      name: 'container',
-      remotes: {
-        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-      },
-      shared: packageJson.dependencies,
-    }),
-  ],
+	mode: 'production',
+	output: {
+		filename: '[name].[contenthash].js',
+		publicPath: '/container/latest/',
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			name: 'container',
+			remotes: {
+				marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+				auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+			},
+			shared: packageJson.dependencies,
+		}),
+	],
 };
 
 module.exports = merge(commonConfig, prodConfig);
