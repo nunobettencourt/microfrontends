5054b770ad6448cc113e8d9825a85eff4d48ec58
Nuno Bettencourt <nuno.bettencourt@playtech.com>
2022-08-16 11:10:38 +0100

    Jamie

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -80,10 +80,7 @@ export default function Album() {
 							Home Page
 						</Typography>
 						<Typography variant="h5" align="center" paragraph>
-							Something short and leading about the collection
-							below—its contents, the creator, etc. Make it short
-							and sweet, but not too short so folks don&apos;t
-							simply skip over it entirely.
+							JAMIE
 						</Typography>
 						<div className={classes.heroButtons}>
 							<Grid container spacing={2} justify="center">
