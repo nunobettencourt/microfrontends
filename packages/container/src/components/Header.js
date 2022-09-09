0aaead12169996208d7373773504c4edee33f3e8
Nuno Bettencourt <nuno.bettencourt@playtech.com>
2022-09-09 11:34:18 +0100

    Revert "added new font"
    This reverts commit 6d6cab9d56bf00ad2184b3a74f75f188028c6127.

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -1,12 +1,56 @@
 import React from 'react';
-import { AppBar, Button, Toolbar, Typography } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import { Link as RouterLink } from 'react-router-dom';
 
-const Header = ({ isSignedIn, onSignOut }) => {
-	const theme = useTheme();
+const useStyles = makeStyles((theme) => {
+	console.log('container: ', theme.palette.type);
+	return {
+		'@global': {
+			ul: {
+				margin: 0,
+				padding: 0,
+				listStyle: 'none',
+			},
+			a: {
+				textDecoration: 'none',
+			},
+		},
+		appBar: {},
+		toolbar: {
+			flexWrap: 'wrap',
+			justifyContent: 'space-between',
+		},
+		link: {
+			margin: theme.spacing(1, 1.5),
+		},
+		heroContent: {
+			padding: theme.spacing(8, 0, 6),
+		},
+		cardPricing: {
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'baseline',
+			marginBottom: theme.spacing(2),
+		},
+		footer: {
+			borderTop: `1px solid ${theme.palette.divider}`,
+			marginTop: theme.spacing(8),
+			paddingTop: theme.spacing(3),
+			paddingBottom: theme.spacing(3),
+			[theme.breakpoints.up('sm')]: {
+				paddingTop: theme.spacing(6),
+				paddingBottom: theme.spacing(6),
+			},
+		},
+	};
+});
 
-	console.log('header: ', theme.palette.mode);
+const Header = ({ isSignedIn, onSignOut }) => {
+	const classes = useStyles();
 
 	const onClick = () => {
 		if (isSignedIn && onSignOut) {
@@ -16,8 +60,8 @@ const Header = ({ isSignedIn, onSignOut }) => {
 
 	return (
 		<>
-			<AppBar position="static" elevation={0}>
-				<Toolbar>
+			<AppBar position="static" elevation={0} className={classes.appBar}>
+				<Toolbar className={classes.toolbar}>
 					<Typography
 						variant="h6"
 						noWrap
@@ -36,6 +80,7 @@ const Header = ({ isSignedIn, onSignOut }) => {
 					</Typography>
 					<Button
 						variant="outlined"
+						className={classes.link}
 						component={RouterLink}
 						to={isSignedIn ? '/' : '/auth/signin'}
 						onClick={onClick}
--- packages/container/src/styles/theme.js
+++ packages/container/src/styles/theme.js
@@ -1,27 +1,26 @@
-import { createTheme } from '@mui/material/styles';
+/**
+ * If you would like to remove the console warning regarding findDOMNode,
+ * comment out the first line that follows this comment and uncomment the second.
+ * Please bare in mind that this warning only occurs in dev mode and, as such,
+ * the first line should be uncommented before a new build.
+ * The reason for this warning lies in the fact that material-ui versions lower
+ * than 5.0 still use findDOMNode  which has been deprecated in strict mode,
+ */
+import { createTheme } from '@material-ui/core';
+//import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createTheme({
 	spacing: 4,
 	palette: {
-		mode: 'dark',
+		type: 'dark',
 	},
 	typography: {
-		fontSize: 9,
-
+		fontSize: 12,
 		body1: {
-			fontFamily: ['Poppins', 'sans-serif'].join(','),
+			fontWeight: 300,
 		},
 		body2: {
 			fontWeight: 300,
-		},
-	},
-	components: {
-		MuiPaper: {
-			styleOverrides: {
-				root: {
-					backgroundColor: '#333 !important',
-				},
-			},
 		},
 	},
 });
